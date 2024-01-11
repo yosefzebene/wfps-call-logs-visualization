82d1d42f38ce7da2d65fc8931a0740630d504c7a
yosef-ZzZ <yosef.zebene1@gmail.com>
2024-01-11 10:45:44 -0600

    Added periodic updating

--- src/App.js
+++ src/App.js
@@ -3,19 +3,51 @@ import { useEffect, useState } from 'react';
 import DataMap from './components/DataMap.js';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+let callLogsApiCallCount = 0;
+
 function App() {
   const [callLogs, setCallLogs] = useState([]);
 
-  useEffect(() => {
-    async function getWFPSCallLogs() {
-      // Add caching
-      const response = await fetch("https://data.winnipeg.ca/resource/yg42-q284.json?$where=call_time > '2024-01-04T00:00:00.000'&$order=call_time DESC&$limit=5");
+  const getWFPSCallLogs = async () => {
+    const todaysDateAtMidnight = new Date(new Date().setHours(-6,0,0,0)).toISOString().slice(0, -1);
+
+    const cachedCallLogs = localStorage.getItem('callLogsCache');
+    const cachedCallLogsData = JSON.parse(cachedCallLogs);
+
+    // Check if the call logs is stored locally and if the data is from todays date from midnight.
+    // If not then request for new call logs since midnight.
+    if (cachedCallLogs && cachedCallLogsData[cachedCallLogsData.length-1].call_time > todaysDateAtMidnight) {
+      // Update the call logs if there is new data since the last
+      const mostRecentDate = cachedCallLogsData[cachedCallLogsData.length-1].call_time;
+
+      const response = await fetch(`https://data.winnipeg.ca/resource/yg42-q284.json?$where=call_time > '${mostRecentDate}'&$order=call_time&$limit=5`);
       const data = await response.json();
+
+      setCallLogs(cachedCallLogsData.concat(data));
+    }
+    else {
+      const response = await fetch(`https://data.winnipeg.ca/resource/yg42-q284.json?$where=call_time > '${todaysDateAtMidnight}'&$order=call_time&$limit=5`);
+      const data = await response.json();
+
+      callLogsApiCallCount++
 
       setCallLogs(data);
     }
+  };
+
+  useEffect(() => {
     getWFPSCallLogs();
-  }, [])
+
+    const timer = setInterval(getWFPSCallLogs, 300000);
+    return () => clearInterval(timer);
+  }, []);
+
+  useEffect(() => {
+    if (callLogs.length > 0)
+      localStorage.setItem('callLogsCache', JSON.stringify(callLogs));
+  }, [callLogs]);
+
+  console.log("Call Logs API request count: " + callLogsApiCallCount);
 
   return (
       <DataMap callLogs={callLogs} />
--- src/components/DataMap.js
+++ src/components/DataMap.js
@@ -1,45 +1,61 @@
-import React, { useCallback } from 'react';
-import Map, { Marker, FullscreenControl } from 'react-map-gl';
+import React, { useCallback, useEffect } from 'react';
+import ReactMapGl, { Marker, FullscreenControl } from 'react-map-gl';
 import mapboxgl from 'mapbox-gl';
 import { useState } from 'react';
 
+const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_KEY;
+const locationApiCache = new Map();
+
 const DataMap = ({callLogs}) => {
-    const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_KEY;
+
+    console.log(callLogs);
 
     const [markers, setMarkers] = useState([]);
-
-    const getCallLocation = async (neighbourhood) => {
-        const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${neighbourhood}.json?proximity=-97.138451,49.895077&access_token=${MAPBOX_TOKEN}`);
-        const data = await response.json();
-
-        return data.features[0].center;
-    }
-
-    const onMapLoad = () => {
-        callLogs.map(async (callLog) => {
-            // Add caching
-            const location = await getCallLocation(callLog.neighbourhood);
-
-            const marker = {
-                id: callLog.incident_number,
-                longitude: location[0],
-                latitude: location[1],
-                incident_type: callLog.incident_type,
-                units: callLog.units,
-                car_accident: callLog.motor_vehicle_incident,
-                closed_time: callLog.closed_time
-            }
-
-            setMarkers(prevArray => [...prevArray, marker]);
-        });
-    }
 
     const popup = useCallback((marker) => {
         return new mapboxgl.Popup().setText(marker.incident_type);
     }, []);
 
+    useEffect(() => {
+        const getCallLocationByNeighbourhood = async (neighbourhood) => {
+            if(!locationApiCache.has(neighbourhood)) {
+                const response = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${neighbourhood}.json?proximity=-97.138451,49.895077&access_token=${MAPBOX_TOKEN}`);
+                const data = await response.json();
+
+                locationApiCache.set(neighbourhood, data.features[0].center);
+            }
+    
+            return locationApiCache.get(neighbourhood);
+        }
+
+        const onCallLogsUpdate = async () => {
+            for (const callLog of callLogs) {
+                const location = await getCallLocationByNeighbourhood(callLog.neighbourhood);
+    
+                const marker = {
+                    id: callLog.incident_number,
+                    longitude: location[0],
+                    latitude: location[1],
+                    incident_type: callLog.incident_type,
+                    units: callLog.units,
+                    car_accident: callLog.motor_vehicle_incident,
+                    closed_time: callLog.closed_time
+                }
+    
+                setMarkers(prevArray => [...prevArray, marker]);
+            };
+        }
+
+        setMarkers([]);
+
+        onCallLogsUpdate();
+    }, [callLogs])
+
+    console.log("Cache size: " + locationApiCache.size);
+    console.log("Call log count: " + callLogs.length);
+
     return (
-        <Map
+        <ReactMapGl
             initialViewState={{
                 longitude: -97.138451,
                 latitude: 49.895077,
@@ -47,7 +63,6 @@ const DataMap = ({callLogs}) => {
                 pitch: 30
             }}
             mapStyle="mapbox://styles/yosefz/clqxzkl9c00pv01pif0z27cm8"
-            onLoad={onMapLoad}
             style={{ height: '100vh', width: '100vw' }}
             mapboxAccessToken={MAPBOX_TOKEN}
             maxBounds={[[-97.639717, 49.704730], [-96.649797, 50.069708]]}
@@ -65,7 +80,7 @@ const DataMap = ({callLogs}) => {
             }
 
             <FullscreenControl position={"top-left"} />
-        </Map>
+        </ReactMapGl>
     );
 };
 
